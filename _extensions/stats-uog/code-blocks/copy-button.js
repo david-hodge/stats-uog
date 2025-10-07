4d8e9ebf6decab535a6c7a7fc40bdb46f4d91384
dhodge180 <tech@djhodge.com>
2025-10-07 15:08:36 +0100

    Fixing issue where R/Python tabs not tabbable to. Quarto book fails, but quarto website doesnt so copying href='' approach

--- _extensions/stats-uog/code-blocks/copy-button.js
+++ _extensions/stats-uog/code-blocks/copy-button.js
@@ -10,3 +10,15 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+// Tabbing to panels doesnt work on all browsers. But adding an empty href="" can help
+// this is done by default in Quarto websites just not books, for some unknown reason
+// Quarto 1.7.32
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".panel-tabset:not(.unstyled) .nav-link").forEach(function(tab){
+    if (!tab.hasAttribute("href")) {
+      tab.setAttribute("href", "");
+    }
+  });
+});
+
